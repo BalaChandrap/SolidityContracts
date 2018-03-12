66e24ae36af3dec220e6fe1b10a07a57ba4ae5b8
balachandra87 <balachandrapulavarthi@gmail.com>
2018-03-12 15:31:39 +0530

    update record method added in students contract

--- students.sol
+++ students.sol
@@ -17,9 +17,15 @@ contract Students {
     
     address facultyAddress;
     
+
+   
+    
+    
     mapping (address => Student) allStudentsData;
     
     event studentRecordAdded(address studentAddress,uint timeStamp);
+    
+    event studentRecordUPdated(address indexed updatedBy,address studentAddress,uint timeStamp);
     
     
     
@@ -28,6 +34,7 @@ contract Students {
         facultyAddress = msg.sender;
     }
     
+   
     
     modifier onlyFaculty() {
         
@@ -35,25 +42,61 @@ contract Students {
         _;
     }
     
+    function changeFaculty(address newFaculty) public onlyFaculty
+    {
+        facultyAddress = newFaculty;
+        
+        
     
-    function addStudentRecord(address _studentAddress,uint _rollNumber,string _studentName,Results _studentResult) public onlyFaculty {
+        
+    
+        
+    }
+    
+    
+    function addStudentRecord(address _studentAddress,uint _rollNumber,string _studentName,Results _studentResult) public onlyFaculty  {
+        
+        require(msg.value>10);
         
         Student memory temp = Student(_rollNumber,sha256(_studentName),_studentResult);
         
         allStudentsData[_studentAddress] = temp;
         
-        studentRecordAdded(_studentAddress,now);
+       emit studentRecordAdded(_studentAddress,now);
         
     }
     
     
-    function getStudentRecord(address _studentAddress) constant public returns(uint,bytes32,Results)  {
+    function getStudentRecord(address _studentAddress) view public returns(uint,bytes32,Results)  {
         
         Student memory temp = allStudentsData[_studentAddress];
         
         return (temp.rollNumber,temp.hash,temp.studentResult);
         
     }
+    
+    modifier checkPermission (address _studentAddress){
+        require(msg.sender==_studentAddress||msg.sender==facultyAddress);
+        require(allStudentsData[_studentAddress].rollNumber!=0);
+        _;
+    }
+    
+    
+    function editStudentRecord(address _studentAddress,string _updatedName,Results _updatedResults) public checkPermission( _studentAddress) {
+        
+        Student memory temp = allStudentsData[_studentAddress];
+        
+        temp.hash = sha256(_updatedName);
+        temp.studentResult = _updatedResults;
+        
+        allStudentsData[_studentAddress] = temp;
+        
+        emit studentRecordUPdated(msg.sender,_studentAddress,now);
+        
+    }
+    
+    
+  
     
     
     
